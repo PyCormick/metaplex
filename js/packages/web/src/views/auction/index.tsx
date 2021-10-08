5d9e45e1de09ebfcac1cd45b1e946dc40b41fbb1
daniil <daniil.agarev@convexitydmcc.com>
2021-10-08 19:06:57 +0400

    Add switch url for switch clusters

--- js/packages/web/src/views/auction/index.tsx
+++ js/packages/web/src/views/auction/index.tsx
@@ -119,7 +119,7 @@ export const AuctionView = () => {
       if (art.mint != null) {
         token_acc = accountByMint.get(art?.mint)
       }
-      getAttributesByNftId(token_acc.pubkey).then(res => {
+      getAttributesByNftId(token_acc.pubkey, env).then(res => {
         setAttributes(res);
         console.log(res)
       }).catch(e => {
